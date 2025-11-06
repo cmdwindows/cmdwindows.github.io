686b8bbb6d0a2df1083c228c7c6bffe33157fa66
cmdwindows <alexandersheilt@gmail.com>
2025-11-06 21:39:55 +0500

    Create app.ts

---
+++ ts/app.ts
@@ -0,0 +1,10 @@
+const div = document.createElement('div');
+
+div.textContent = 'test typescript';
+
+div.style.backgroundColor = 'red';
+div.style.color = 'white';
+div.style.padding = '10px';
+div.style.fontFamily = 'Arial, sans-serif';
+
+document.body.appendChild(div);
